2fdc66d0a20d533e6b9fd3f79bb454e81820862c
Evandro Ribeiro <ribeiroevandro@live.com>
2020-08-21 02:47:01 -0300

    chore: add link to room

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
 import { Title } from './styles';
 
 const Home: React.FC = () => {
-  return <Title>Home</Title>;
+  return (
+    <>
+      <Title>Home</Title>
+      <Link to="/room">Room</Link>
+    </>
+  );
 };
 
 export default Home;
